2d5364e11b9c4879cac92322abc2f7b777c642b6
aamir.shakir <aamir.shakir@aperto.de>
2020-03-30 16:09:07 +0200

    Question

--- src/app/questions/questions.ts
+++ src/app/questions/questions.ts
@@ -1,21 +1,36 @@
 import {buildQuestionary} from "../shared/QuestionaryBuilder";
-
+export enum Gender {
+  Male,
+  Female,
+}
 export const part1 = buildQuestionary("part1")
   .addQuestionContainer("card1", c => c
-    .askText("q1", c => c.defaultTo("hide"))
-    .askText("q2", f => f
-      .hideIf(ctx => ctx.get("q1") === "hide")
-      .showHint()
-    )
-    .askYesNoQuestion("q3", f => f
-      .insteadOfYesSay("hello World!"))
-    .printInfo("q4", f => f.showHint())
-    .askForDate("q5", f => f.showAsPopup())
-    .askMultipleChoiceQuestion("q4", c => c.option("option1", "1").option("option2", "2"))
+    .askText("firstname")
+    .askText("name", f => f
+      .hideIf(ctx => ctx.get("q1") === "hide"))
+    .askYesNoQuestion("q_birthname")
+    .askText("birthname", f => f
+      .hideIf(ctx => ctx.get("q_birthname") !== true))
+    .askMultipleChoiceQuestion("sex", c => c.option("male", "Gender.Male").option("female", "1"))
+    .askForDate("birthdate", f => f.showAsPopup())
+    .askText("birthplace", f => f.showHint())
+    .askMultipleChoiceQuestion("foreveralone", c => c.option("alone", "1").option("married", "2").option("seperated", "3")
+      .option("weathered", "4").option("devoced", "5"))
+    .askForDate("status_since", f => f
+      .hideIf(ctx => ctx.get("foreveralone") === 1 || ctx.get("foreveralone") === null)
+      .showAsPopup())
+    .askYesNoQuestion("german_nationality")
+    .askText("nationality", f => f
+      .hideIf(ctx => ctx.get("german_nationality") !== false)
+      .showHint())
+    .askText("partner_nationality", f => f
+      .hideIf(ctx => ctx.get("foreveralone") === "5"))
+    .askText("tax_ident", f => f.showHint())
   )
   .addQuestionContainer("card2", c => c
+    .askYesNoQuestion("q_parents")
     .askText("q3", f => f
-      .hideIf(ctx => ctx.get("q1", "card1") === "hide")
+      .hideIf(ctx => ctx.get("firstname", "card1") === "hide")
       .defaultTo("Test Q3")
     )
   )
