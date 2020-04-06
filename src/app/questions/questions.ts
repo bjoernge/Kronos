ea649e07b8975d8bb1a5ca3793d6557af2294dfb
Aamir <aamirshakir0@gmail.com>
2020-04-06 12:10:02 +0200

    Added questions and another structure

--- src/app/questions/questions.ts
+++ src/app/questions/questions.ts
@@ -6,7 +6,43 @@ export enum Gender {
 }
 
 export const part1 = buildQuestionary("part1")
-  .addQuestionContainer("card1", c => c
+  .addQuestionContainer("intro", c => c
+    .askMultipleChoiceQuestion("phase", c => c
+      .option("school", 1)
+      .option("uni", 2)
+      .option("practical", 3))
+    .askForDate("start_date", c => c
+      .showAsPopup()
+      .showHint())
+    .askMultipleChoiceQuestion("time", c => c
+      .option("1", 1)
+      .option("2", 2)
+      .option("3", 3)
+      .option("4", 4)
+      .option("5", 5)
+      .option("6", 6)
+      .option("7", 7)
+      .option("8", 8)
+      .option("9", 9)
+      .option("10", 10)
+      .option("11", 11)
+      .option("12", 12)
+      .showHint()
+    )
+    .askYesNoQuestion("aboard")
+    .askYesNoQuestion("parents", c => c
+      .showHint())
+    .askYesNoQuestion("firsttime", c => c
+      .showHint())
+    .askText("institute", c => c
+      .hideIf(ctx => ctx.is("firsttime", null, false)))
+    .askText("number", c => c
+      .hideIf(ctx => ctx.is("firsttime", null, false)))
+  )
+
+  .addQuestionContainer("", c => c)
+
+  .addQuestionContainer("about_me", c => c
     .askText("firstname")
     .askText("name", f => f
       .hideIf(ctx => ctx.is("q1", "hide")))
@@ -29,28 +65,22 @@ export const part1 = buildQuestionary("part1")
     .askText("nationality", f => f
       .hideIf(ctx => ctx.is("german_nationality", true, null))
       .showHint())
-    .askText("partner_nationality", f => f
-      .hideIf(ctx => ctx.is("foreveralone", 1, 3, 4, null)))
+    .askYesNoQuestion("children", f => f
+      .showHint())
     .askText("tax_ident", f => f.showHint())
   )
 
-  .addQuestionContainer("card2", c => c
-    .askYesNoQuestion("q_parents")
-    .printInfo("main_adress", f => f
-      .hideIf(ctx => ctx.is("q_parents", null)))
-    .askText("street", f => f
-      .hideIf(ctx => ctx.is("q_parents", null)))
-    .askText("zip", f => f
-      .hideIf(ctx => ctx.is("q_parents", null)))
-    .askText("city", f => f
-      .hideIf(ctx => ctx.is("q_parents", null)))
+  .addQuestionContainer("mine_adress", c => c
+    .printInfo("main_adress")
+    .askText("street")
+    .askText("zip")
+    .askText("city")
     .askText("out_of_country", f => f
-      .hideIf(ctx => ctx.is("q_parents", null))
       .showHint()
       .defaultTo("DE"))
     .askText("bell", f => f
-     .hideIf(ctx => ctx.is("q_parents", null))
      .showHint())
+    .askYesNoQuestion("q_parents")
     .askYesNoQuestion("know_adress", f => f
       .hideIf(ctx => ctx.is("q_parents", true, null)))
     .printInfo("sec_adress", f => f
@@ -68,10 +98,50 @@ export const part1 = buildQuestionary("part1")
    .askText("sec_bell", f => f
       .hideIf(ctx => ctx.is("know_adress", false, null) || ctx.is("q_parents", true, null))
       .showHint())
-
-    .askText("q3", f => f
-      .hideIf(ctx => ctx.is_n("firstname", "card1", "hide"))
-      .defaultTo("Test Q3")
-    )
+    .askYesNoQuestion("prop_of_parents", f => f
+      .hideIf(ctx => ctx.is("know_adress", false, null) || ctx.is("q_parents", true, null)))
   )
+
+  .addQuestionContainer("capital", c => c
+    .printInfo("capital")
+    .askYesNoQuestion("bar")
+    .askText("bar_amount", c => c
+      .hideIf(ctx => ctx.is("bar", false, null)))
+    .askYesNoQuestion("bank")
+    .askText("bank_amount", c => c
+      .hideIf(ctx => ctx.is("bank", false, null)))
+    .askYesNoQuestion("building_savings")
+    .askText("building_savings_amount", c => c
+      .hideIf(ctx => ctx.is("building_savings", false, null)))
+    .askYesNoQuestion("retirement")
+    .askText("retirement_amount", c => c
+      .hideIf(ctx => ctx.is("retirement", false, null)))
+    .askYesNoQuestion("kfz")
+    .askText("kfz_amount", c => c
+      .hideIf(ctx => ctx.is("kfz", false, null))
+      .showHint())
+    .askYesNoQuestion("stocks")
+    .askText("buisness_assets", c => c
+      .hideIf(ctx => ctx.is("stocks", false, null))
+      .showHint())
+    .askText("stock", c => c
+      .hideIf(ctx => ctx.is("stocks", false, null))
+      .showHint())
+    .askYesNoQuestion("land")
+    .askText("forest", c => c
+      .hideIf(ctx => ctx.is("land", false, null))
+      .showHint())
+    .askText("vacant", c => c
+      .hideIf(ctx => ctx.is("land", false, null))
+      .showHint())
+    .askText("built", c => c
+      .hideIf(ctx => ctx.is("land", false, null))
+      .showHint())
+    .askYesNoQuestion("other")
+    .askText("pension", c => c
+      .hideIf(ctx => ctx.is("other", false, null))
+      .showHint())
+    .askText("other_amount", c => c
+      .hideIf(ctx => ctx.is("other", false, null)))
+  ) 
   .build();
